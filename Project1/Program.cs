9c125223a105d4a463484db2398067a86ca78464
Praze-hub <godspraiseokechukwu07@gmail.com>
2022-04-05 05:02:58 +0100

    task 1

---
+++ Project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello I am Okechukwu God'spraise I am in the backend track of Wella Schools ");
+            Console.WriteLine("I am learning backend development so that I can contribute to the betterment of my society");
+        }
+    }
+}
